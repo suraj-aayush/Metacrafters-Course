1d0634db4518e8ca03af6456bcb502ab8c591c31
Aayush Kumar <105626112+suraj-aayush@users.noreply.github.com>
2024-02-27 23:10:14 +0530

    Update assessment.sol

--- ETH + AVAX/assessment.sol
+++ ETH + AVAX/assessment.sol
@@ -1,2 +1,23 @@
-added
+pragma solidity ^0.8.18;
 
+contract OwnershipContract {
+    address public owner;
+
+    constructor() {
+        owner = msg.sender;
+    }
+
+    function onlyOwner() public view {
+        require(msg.sender == owner, "Only the owner can call this function.");
+    }
+
+    function onwerHere() public view {
+        if(msg.sender!= owner){
+            revert("The caller is not the owner.");
+        }
+    }
+
+    function Owner() public view {
+        assert(msg.sender == owner);
+    }
+}
